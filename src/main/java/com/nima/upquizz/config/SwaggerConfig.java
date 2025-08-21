03f66cc629c7b288768808ec2c435be58fe3b43b
NimaKhajehpour <nima1038khajehpour@gmail.com>
2025-08-21 14:04:26 +0430

    final touches

--- src/main/java/com/nima/upquizz/config/SwaggerConfig.java
+++ src/main/java/com/nima/upquizz/config/SwaggerConfig.java
@@ -5,10 +5,16 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "Api Documentation", version = "v1"), security = @SecurityRequirement(name = "bearerAuth"))
+@OpenAPIDefinition(info = @Info(title = "Api Documentation", version = "v1"),
+        security = @SecurityRequirement(name = "bearerAuth"),
+        servers = {
+        @Server(url = "https://upquizz.up.railway.app", description = "production server")
+        }
+)
 @SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "JWT")
 public class SwaggerConfig {
 }
